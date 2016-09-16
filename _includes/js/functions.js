4bc1e6d662e0c07bbbba9711f8fc17046b64e27b
Amir R Muntasser <arkmuntasser@gmail.com>
2016-09-15 19:52:38 -0700

    added conditionsls and functions

---
+++ _includes/js/functions.js
@@ -0,0 +1,95 @@
+// Return early from functions
+// Bad example
+function isPercentage(val) {
+  if (val >= 0) {
+    if (val < 100) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
+
+// Better example
+function isPercentage(val) {
+  if (val < 0) {
+    return false;
+  }
+
+  if (val > 100) {
+    return false;
+  }
+
+  return true;
+}
+
+// Even better example
+function isPercentage(val) {
+  var isInRange = (val >= 0 && val <= 100);
+  return isInRange;
+}
+
+
+// Name your closures
+// Bad example
+req.on('end', function() {
+  console.log('losing');
+});
+
+// Good example
+req.on('end', function onEnd() {
+  console.log('winning');
+});
+
+
+// No nested closures
+// Bad example
+setTimeout(function() {
+  client.connect(function() {
+    console.log('losing');
+  });
+}, 1000);
+
+// Good example
+setTimeout(function() {
+  client.connect(afterConnect);
+}, 1000);
+
+function afterConnect() {
+  console.log('winning');
+}
+
+// Method chaining
+// Bad example
+User
+.findOne({ name: 'foo' })
+.populate('bar')
+.exec(function(err, user) {
+  return true;
+});
+
+User.findOne({ name: 'foo' })
+  .populate('bar')
+  .exec(function(err, user) {
+    return true;
+  });
+
+User.findOne({ name: 'foo' }).populate('bar')
+.exec(function(err, user) {
+  return true;
+});
+
+User.findOne({ name: 'foo' }).populate('bar')
+  .exec(function(err, user) {
+    return true;
+  });
+
+// Good example
+User
+  .findOne({ name: 'foo' })
+  .populate('bar')
+  .exec(function(err, user) {
+    return true;
+  });
