5024a8f41e30e90bb78c0e2bf71c2680ba5edf6f
Amir R Muntasser <arkmuntasser@gmail.com>
2016-09-15 19:29:49 -0700

    text fixes

--- _includes/js/naming-conventions.js
+++ _includes/js/naming-conventions.js
@@ -1,33 +1,30 @@
-// variables, properties and function names
-
-// Bad example
+/* Variables, Properties and Function names */
+/* Bad example */
 var admin_user = db.query('SELECT * FROM users ...');
 
-// Good example
+/* Good example */
 var adminUser = db.query('SELECT * FROM users ...');
 
 
-// class names
-
-// Bad example
+/* Class names */
+/* Bad example */
 function bank_Account() {
 }
 
-// Good example
+/* Good example */
 function BankAccount() {
 }
 
 
-// constants
-
-// Bad example
+/* Constants */
+/* Bad example */
 const SECOND = 1 * 1000;
 
 function File() {
 }
 File.fullPermissions = 0777;
 
-// Good example
+/* Good example */
 var SECOND = 1 * 1000;
 
 function File() {
